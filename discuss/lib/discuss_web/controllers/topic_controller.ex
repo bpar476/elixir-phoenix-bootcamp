f748524ea91e3ff621594a8cac108b9e128f707f
Ben Partridge <ben.partridge@immutable.com>
2023-01-19 11:05:02 +1100

    Elixir Phoenix Bootcamp Progress
    Part 1 - Card Module ✅
    Part 2 - Identicon Generator ✅
    Part 3 - Forum Phoenix App <in progress>

---
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,13 @@
+defmodule DiscussWeb.TopicController do
+  # Controllers, models, and views have a use statement like this
+  # So that they can "inherit" default behaviour from a def in the
+  # DiscussWeb module
+  use DiscussWeb, :controller
+
+  @doc """
+  Renders a form to create a new topic
+  """
+  def new(conn, params) do
+    
+  end
+end
